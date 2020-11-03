f52b61751a97daec1e7da15e2613bc63cc4687f3
Junior-Shyko <franciscoanto@gmail.com>
2020-11-02 22:25:36 -0300

    Lista de usuários

--- src/pages/TableList.vue
+++ src/pages/TableList.vue
@@ -10,10 +10,28 @@
               <h4 class="card-title">Lista de usuários</h4>
               <p class="card-category">Lista com o nome de todos usuários cadastrados</p>
             </template>
-            <!-- <l-table class="table-hover table-striped table-sm"
-                     :columns="table1.columns"
-                     :data="table1.data">
-            </l-table> -->
+            <table class="table">
+              <thead class="thead-dark">
+                <tr>
+                  <th scope="col">ID</th>
+                  <th scope="col">Nome</th>
+                  <th scope="col">E-mail</th>
+                  <th scope="col">Cidade</th>
+                  <th scope="col">Ação</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for='user in users' :key="user.userWebservice_id">
+                  <th scope="row">{{user.userWebservice_id}}</th>
+                  <td>{{user.userWebservice_name}}</td>
+                  <td>{{user.userWebservice_email}}</td>
+                  <td>{{user.userWebservice_city}}</td>
+                  <td>Editar</td>
+                </tr>
+                
+              </tbody>
+            </table>
+
           </card>
         </div>
       </div>
@@ -31,15 +49,16 @@
     },
     data () {
       return {
-        
+        users: []
       }
     },
     methods: {
       allUsers: function() {
-        axios.get('http://134.122.90.6:4000/api/webservice/users')
-        .then(function (response) {
+        axios.get('http://192.168.10.22:4000/api/webservice/users')
+        .then(response => {
           // handle success
           console.log(response.data)
+          this.users = response.data
         })
         .catch(function (error) {
           // handle error
